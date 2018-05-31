88976fb9b609542c09aa5777471ee86b939dcd9c
MaQi <383930056@qq.com>
2018-05-31 17:32:37 +0800

    下班提交

--- app/src/main/java/xiaomakj/wificlock/com/common/UrlConstant.kt
+++ app/src/main/java/xiaomakj/wificlock/com/common/UrlConstant.kt
@@ -4,7 +4,8 @@ package xiaomakj.wificlock.com.common
  * Created by Administrator on 2018/5/18.
  */
 //http://192.168.1.105/index.php/api/name/run
-const val BASEURL = "http://192.168.1.140/"
+//const val BASEURL = "http://192.168.1.140/"
+const val BASEURL = "http://wifi.xiaomakj.cn/wificlock/public/"
 const val TEST = "index.php/api/name/run"
 const val ADDCLOCKRECORD = "index.php/admin/wifi/clock/record/add"
 const val LOGIN = "index.php/api/user/login"
--- app/src/main/java/xiaomakj/wificlock/com/mvp/presenter/ClockRecordPresenter.kt
+++ app/src/main/java/xiaomakj/wificlock/com/mvp/presenter/ClockRecordPresenter.kt
@@ -3,16 +3,13 @@ package xiaomakj.wificlock.com.mvp.presenter
 
 import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.text.format.DateFormat
-import android.widget.ImageView
 import android.widget.TextView
 import com.aspsine.irecyclerview.universaladapter.ViewHolderHelper
 import com.aspsine.irecyclerview.universaladapter.recyclerview.CommonRecycleViewAdapter
 import com.bumptech.glide.Glide
 import org.jetbrains.anko.toast
 import xiaomakj.wificlock.com.R
-import javax.inject.Inject
 import xiaomakj.wificlock.com.api.AppApi
 import xiaomakj.wificlock.com.api.BaseObserver
 import xiaomakj.wificlock.com.common.BASEURL
@@ -24,8 +21,7 @@ import xiaomakj.wificlock.com.mvp.contract.ClockRecordContract
 import xiaomakj.wificlock.com.mvp.ui.activity.ClockRecordActivity
 import xiaomakj.wificlock.com.utils.GlideCircleTransform
 import xiaomakj.wificlock.com.utils.SharedPreferencesUtil
-import xiaomakj.wificlock.com.utils.Utils
-import java.util.*
+import javax.inject.Inject
 
 class ClockRecordPresenter @Inject constructor(private val appApi: AppApi, private val context: Context) :
         RxPresenter<ClockRecordContract.View, ActivityClockRecordBinding>(), ClockRecordContract.Presenter {
@@ -39,6 +35,7 @@ class ClockRecordPresenter @Inject constructor(private val appApi: AppApi, priva
             override fun convert(helper: ViewHolderHelper, data: ClockRecordDatas.ClockList, position: Int) {
                 helper.getView<TextView>(R.id.createtime).text = DateFormat.format("yyyy-MM-dd:HH:mm", data.createtime*1000)
                 helper.getView<TextView>(R.id.username).text = loginDatas.nickname
+                helper.getView<TextView>(R.id.clock_place).text = data.clock_place
                 helper.getView<TextView>(R.id.wifiname).text = data.wifiname ?: "未知"
                 helper.getView<TextView>(R.id.wifi_distance).text = "路由器距离:${data.wifi_distance}m"
                 helper.getView<TextView>(R.id.gps_distance).text = "公司的距离:${data.gps_distance}m"
